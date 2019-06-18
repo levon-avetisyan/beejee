e5713e6bac6b6132829b6aa9ee4075220304ac15
levon-avetisyan <levon.s.avetisyan@gmail.com>
2019-06-18 20:10:12 +0400

    asd

---
+++ client/build/precache-manifest.aec140798a822a0ed3527eb02f461c29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "136c4aff3b44cd3f09c9837e615a1b4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7831021c517fde347ef0",
+    "url": "/static/css/main.c16f4a8d.chunk.css"
+  },
+  {
+    "revision": "c47a4f8a20679537c821",
+    "url": "/static/js/2.c8e58e13.chunk.js"
+  },
+  {
+    "revision": "7831021c517fde347ef0",
+    "url": "/static/js/main.970d5493.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
