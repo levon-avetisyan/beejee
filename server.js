dca8bb677a66505dd7b322f29e3ec9effab38276
levon-avetisyan <levon.s.avetisyan@gmail.com>
2019-06-18 17:49:04 +0400

    1ss

--- server.js
+++ server.js
@@ -40,10 +40,10 @@ app.use('/edit', edit);
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, "client", "build")));
 
-    app.get(/^\/(?!api).*/, (req, res) => {
-        res.sendFile(path.join(__dirname, './client/build/index.html'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
     });
     console.log('Serving React App...');
 }
