80d813926d756064f61b00e3b6a968dd961131d6
levon-avetisyan <levon.s.avetisyan@gmail.com>
2019-06-18 17:45:30 +0400

    1s

--- server.js
+++ server.js
@@ -42,9 +42,10 @@ if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    app.get(/^\/(?!api).*/, (req, res) => {
+        res.sendFile(path.join(__dirname, './client/build/index.html'));
     });
+    console.log('Serving React App...');
 }
 const port = process.env.PORT || 5000;
 
