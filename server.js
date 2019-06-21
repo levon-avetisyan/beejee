9574dd6cd4828767078a9b08285b53ac1725d74b
levon-avetisyan <levon.s.avetisyan@gmail.com>
2019-06-21 15:56:48 +0400

    ad

--- server.js
+++ server.js
@@ -6,17 +6,16 @@ const passport = require('passport');
 const path = require('path');
 const port = process.env.PORT || 5000;
 
-app.use('/', express.static(path.join(__dirname, 'client/build')));
-app.use('/dayside', express.static(path.join(__dirname, 'client/build/dayside')));
-
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('client/build'));
+    app.use('/',express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
+
+    app.use('/dayside', express.static(path.join(__dirname, 'client/build/dayside')));
 
     app.get('/dayside', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'dayside', 'index.php'));
